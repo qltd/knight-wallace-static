5a5e7aa8dc93d9002069a3dccc6f683b3796a66b
Ramsey <rdarling42@gmail.com>
2015-11-18 14:01:37 -0500

    adding some menu interactivity to the main nav

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(document).ready(function(){
+    
+    $('#main_nav .primary').hover(function(){
+        var menu = $(this).attr('data-sub-nav-menu');
+        $('.sub-nav-wrap ul').addClass('disappear');
+        $('.sub-nav-wrap .'+menu).removeClass('disappear');
+    },function(){
+        //not doing anything atm 
+    });
+
+});
