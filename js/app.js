3219e0585a1fe3410b45f3c1fbb58c8ad6fcf9d7
Ramsey <rdarling42@gmail.com>
2015-11-18 14:15:14 -0500

    returning to active menu on mouse out

--- js/app.js
+++ js/app.js
@@ -5,11 +5,15 @@ $(document).foundation();
 $(document).ready(function(){
     
     $('#main_nav .primary').hover(function(){
+        //on mouse in
         var menu = $(this).attr('data-sub-nav-menu');
         $('.sub-nav-wrap ul').addClass('disappear');
         $('.sub-nav-wrap .'+menu).removeClass('disappear');
     },function(){
-        //not doing anything atm 
+        //on mouse out
+        var menu = $('#main_nav .primary.active').attr('data-sub-nav-menu');
+        $('.sub-nav-wrap ul').addClass('disappear');
+        $('.sub-nav-wrap .'+menu).removeClass('disappear');
     });
 
 });
