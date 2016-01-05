238a746b02c0b8e675de02050c72011579c8daa4
Ramsey <rdarling42@gmail.com>
2016-01-05 15:57:45 -0500

    working on improving hover menu functionality

--- js/app.js
+++ js/app.js
@@ -3,10 +3,13 @@
 $(document).foundation();
 
 $(document).ready(function(){
-    
+    var active_menu;
+
     $('#main_nav .primary').hover(function(){
         //on mouse in
         var menu = $(this).attr('data-sub-nav-menu');
+        $('#main_nav .primary').removeClass('active'); 
+        $(this).addClass('active');
         $('.sub-nav-wrap ul').addClass('disappear');
         $('.sub-nav-wrap .'+menu).removeClass('disappear');
     },function(){
@@ -14,6 +17,16 @@ $(document).ready(function(){
         var menu = $('#main_nav .primary.active').attr('data-sub-nav-menu');
         $('.sub-nav-wrap ul').addClass('disappear');
         $('.sub-nav-wrap .'+menu).removeClass('disappear');
+    });
+
+    $('#main_nav').hover(function(active_menu){
+        //on mouse in 
+        //grab original active menu 
+        active_menu = $(this).attr('data-sub-nav-menu');
+    },function(active_menu){
+        //on mouse out 
+        $('#main_nav .primary').removeClass('active'); 
+        $('#main_nav .primary'+active_menu).addClass('active');
     });
 
 });
